572206c6bf0ce7760d490e3501c2ac434b2b6db0
SandunikaH <sandunikahansinie@gmail.com>
2019-02-16 22:24:02 +0530

    Login component
    Signed-off-by: SandunikaH <sandunikahansinie@gmail.com>

---
+++ src/Login.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+import './css/Login.css';
+
+import axios from 'axios'
+
+class Login extends Component
+{
+
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            email: '',
+            password: '',
+            failedAttempt: false
+        };
+        this.PerformLogin = this.PerformLogin.bind(this);
+        this.handleEmailChange = this.handleEmailChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+
+
+    }
+
+    PerformLogin(){
+        axios.post('http://localhost:8080/users/login', {
+            email: this.state.email,
+            password: this.state.password,
+        })
+            .then(response => {
+                this.setState({failedAttempt: false});
+            })
+            .catch(error => {
+                this.setState({failedAttempt: true});
+            })
+    }
+
+    handleEmailChange(event) {
+        this.setState({email: event.target.value});
+    }
+
+    handlePasswordChange(event) {
+        this.setState({password: event.target.value});
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-4 col-sm-2 col-xs-3"></div>
+                    <div className="col-md-4 col-sm-8 col-xs-6">
+                        <div className="panel panel-default">
+                            <div className="panel-body">
+                                <div className="form-group">
+                                    <label>User Name </label>
+                                    <div className="icon-holder">
+                                        <input type="text"
+                                               onChange={this.handleEmailChange}
+                                               className="form-control"/>
+                                    </div>
+                                </div>
+                                <div className="form-group">
+                                    <label>Password</label>
+                                    <div className="icon-holder">
+                                        <input type="password"
+                                               onChange={this.handlePasswordChange}
+                                               className="form-control"/>
+                                    </div>
+                                </div>
+                            </div>
+                            {this.state.failedAttempt &&
+                            <div className="alert alert-danger" role="alert">
+                                Invalid username password combination
+                            </div>
+                            }
+                            <div className="panel-footer">
+                                <button onClick={this.PerformLogin} className="btn btn-success btn-block">Login</button>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className="col-md-4 col-sm-2 col-xs-3"></div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Login;
