af5733f7a33ade99ee317da9fe7b5f4a73e808bb
SandunikaH <sandunikahansinie@gmail.com>
2019-03-11 14:39:34 +0530

    modifications on order details modal

--- src/Orders.js
+++ src/Orders.js
@@ -2,16 +2,24 @@ import React, { Component } from 'react';
 import './css/Order.css'
 import User from "./User";
 import {Header} from "./components/Header";
+import OrderDetails from "./components/OrderDetails";
 import axios from "axios";
 
 export class Orders extends Component{
 
     constructor(props, context) {
         super(props, context);
+
+        this.replaceModalItem = this.replaceModalItem.bind(this);
+        this.saveModalDetails = this.saveModalDetails.bind(this);
+
         this.state = {
             validating: true,
-            orders: []
+            orders: [],
+            requiredOrder: 0,
+            modeIsOpen: false
         };
+
         User.ValidateAccessToken()
             .then((status)=> {
                 this.setState({validating: false});
@@ -31,42 +39,74 @@ export class Orders extends Component{
             });
     }
 
+    replaceModalItem(index) {
+        this.setState({
+            requiredOrder: index
+        });
+    }
+
+    toggleModal() {
+        this.setState({
+            modeIsOpen: !this.state.modeIsOpen
+        });
+    }
+
+    saveModalDetails(item) {
+        const requiredItem = this.state.requiredOrder;
+        let tempOrder = this.state.orders;
+        tempOrder[requiredItem] = item;
+        this.setState({ orders: tempOrder });
+    }
+
+    deleteOrder(toBeDeleted) {
+        console.log(toBeDeleted);
+    }
+
     render() {
         if(this.state.validating){
             return (
                 <div>Please wait</div>
             );
         }
+
+        const orderDetail = this.state.orders.map((order, index) => (
+            <div className="item" key={index}>
+                <div>
+                    <div className="orderNo">
+                        Order No: <tab2>{index+1}</tab2> <tab1>Order ID: <tab2>{order.orderId}</tab2></tab1>
+                    </div>
+                    <div className="buttonsPane">
+                        <button className="btn btn-info" >
+                            <i className="glyphicon glyphicon-new-window"></i>
+                        </button> <tab2></tab2>
+                        <button className="btn btn-danger">
+                            <i className="glyphicon glyphicon-trash"></i>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        ));
+
+        let modalData = this.state.orders[this.state.requiredOrder];
+
         return (
             <div>
                 <Header/>
                 <div className="container">
                     <h3>Orders</h3>
-                    <h4>Order Count: <b>{this.state.orders.length}</b></h4>
+                    {
+                        this.state.orders.length === 0 &&
+                        <h4>No orders found</h4>
+                    }
+                    {
+                        this.state.orders.length > 0 &&
+                        <h4>Orders Count: <b>{this.state.orders.length}</b></h4>
+                    }
                     <div className="order-list">
-                        {
-                            this.state.orders.map(function(d, idx){
-                                return (
-                                    <div className="item" key={idx}>
-                                        <div>
-                                            <div className="orderNo">
-                                                Order No: <tab2>{idx+1}</tab2> <tab1>Order ID: <tab2>{d.orderId}</tab2></tab1>
-                                            </div>
-                                            <div className="buttonsPane">
-                                                <a href="#" className="btn btn-info">
-                                                    <i className="glyphicon glyphicon-new-window"></i>
-                                                </a> <tab2></tab2>
-                                                <a href="#" className="btn btn-danger">
-                                                    <i className="glyphicon glyphicon-trash"></i>
-                                                </a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                );
-                            })
-                        }
+                        {orderDetail}
                     </div>
                 </div>
+                <OrderDetails/>
             </div>
         );
     }
--- src/components/OrderDetails.js
+++ src/components/OrderDetails.js
@@ -1,6 +1,49 @@
 import React from "react";
 import '../css/OrderDetails.css';
+import {Modal, ModalBody, ModalFooter} from "react-bootstrap";
 
-// export class OrderDetails extends React.Component{
-//
-// }
+class OrderDetails extends React.Component{
+
+    constructor(props, context) {
+        super(props, context);
+
+        this.handleSave = this.handleSave.bind(this);
+
+        this.state = {
+            productList: []
+        }
+    }
+
+    componentWillReceiveProps(nextProps) {
+        this.setState({
+            productList: nextProps.productList
+        });
+    }
+
+    titleHandler(e) {
+        this.setState({ title: e.target.value });
+    }
+
+    msgHandler(e) {
+        this.setState({ msg: e.target.value });
+    }
+
+    handleSave() {
+        const item = this.state;
+        this.props.saveModalDetails(item)
+    }
+
+    render() {
+        return (
+            <Modal isOpen={true}>
+                <ModalBody>Modal Body</ModalBody>
+                <ModalFooter>
+                    <button className="btn btn-info">Save</button>
+                    <button className="btn btn-danger">Close</button>
+                </ModalFooter>
+            </Modal>
+        );
+    }
+}
+
+export default OrderDetails;
